836f905c396923d314c003db122bb7aa4cd6e98f
nayyaung9 <naygit.developer@gmail.com>
2021-04-30 13:58:09 +0630

    fix: change online api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const apiEndpoint = "https://api-discord-clone.herokuapp.com";
-export const apiEndpoint = 'http://localhost:8000';
+export const apiEndpoint = "https://api-discord-clone.herokuapp.com";
+// export const apiEndpoint = 'http://localhost:8000';
 
 export default axios.create({
   baseURL: apiEndpoint,
